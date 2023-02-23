2aff11fbbe26b3c696e0aee1bb8441d6fa607888
Roham <Rohamgames@gmail.com>
2023-02-23 15:00:27 +0000

    -UPDATED TO PROPER DOWNLOAD LINK

--- script/download-tbb.js
+++ script/download-tbb.js
@@ -35,14 +35,14 @@ exports.getTorBrowserLink = function(platform, version, callback) {
 
     switch (platform) {
       case 'win32':
-        return `${link}/torbrowser-install-${v}_en-US.exe`;
+        return `${link}/torbrowser-install-${v}_ALL.exe`;
       case 'darwin':
-        return `${link}/TorBrowser-${v}-osx64_en-US.dmg`;
+        return `${link}/TorBrowser-${v}-osx64_ALL.dmg`;
       case 'android':
       case 'linux':
         return os.arch() === 'x64'
-          ? `${link}/tor-browser-linux64-${v}_en-US.tar.xz`
-          : `${link}/tor-browser-linux32-${v}_en-US.tar.xz`
+          ? `${link}/tor-browser-linux64-${v}_ALL.tar.xz`
+          : `${link}/tor-browser-linux32-${v}_ALL.tar.xz`
       default:
         throw new Error(`Unsupported platform "${platform}"`);
     }
@@ -249,7 +249,7 @@ exports.install = function(callback) {
                 break;
               case 'android':
               case 'linux':
-                rimraf.sync(path.join(BIN_DIR, 'tor-browser_en-US'));
+                rimraf.sync(path.join(BIN_DIR, 'tor-browser_ALL'));
                 break;
               default:
             }
